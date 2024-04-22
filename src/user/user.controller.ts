075f64587eb8942047b9c857b1f96711a0ed9a72
Keson.Cai <keson.cai@kerryprops.com>
2024-04-22 17:23:28 +0800

    完成注册功能

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,35 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Query, Inject } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterDto } from './dto/register-dto';
+import { EmailService } from 'src/email/email.service';
+import { RedisService } from 'src/redis/redis.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Inject(RedisService)
+  private readonly redisService: RedisService;
+
+  @Inject(EmailService)
+  private readonly emailService: EmailService;
+
   @Post('register')
   async register(@Body() registerDto: RegisterDto) {
     return await this.userService.register(registerDto);
+  }
+
+  @Get('register-captcha')
+  async captcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(`captcha_${address}`, code, 5 * 60);
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '注册验证码',
+      html: `<p>你的注册验证码是 ${code}</p>`,
+    });
+    return '发送成功';
   }
 }
