688b3f6acf60382f5dd4dfe6b9ae72c8e8a532bf
Keson.Cai <keson.cai@kerryprops.com>
2024-04-23 16:47:19 +0800

    完成鉴权

--- src/permission.guard.ts
+++ src/permission.guard.ts
@@ -1,11 +1,45 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class PermissionGuard implements CanActivate {
+  @Inject(Reflector)
+  private readonly reflector: Reflector;
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const requirePermissions = this.reflector.getAllAndOverride(
+      'require-permission',
+      [context.getClass(), context.getHandler()],
+    );
+
+    if (!requirePermissions) {
+      return true;
+    }
+    const request: Request = context.switchToHttp().getRequest();
+
+    if (!request.user) {
+      return true;
+    }
+
+    const userPermissions = request.user.permissions;
+
+    for (let i = 0; i < requirePermissions.length; i++) {
+      const curPermission = requirePermissions[i];
+      const found = userPermissions.find((item) => item.code === curPermission);
+      if (!found) {
+        throw new UnauthorizedException('您没有访问该接口的权限');
+      }
+    }
     return true;
   }
 }
