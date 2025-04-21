020eb57282133ece972bb0a675885617772448d6
caisenyuan <923670795@qq.com>
2025-04-21 22:07:37 +0800

    修改配置

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,6 @@ async function bootstrap() {
   app.useGlobalFilters(new CustomExceptionFilter());
 
   app.enableCors({
-    // origin: '*', // 允许任何来源 - 开发时可以，生产环境建议指定明确来源
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // 允许的方法
     credentials: true, // 允许携带凭证 (例如 cookies, Authorization header)
